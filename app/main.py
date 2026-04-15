75d28f0ee62f44e87255eaacab24e725b23eabd9
Oliver Catania <oacatania@gmail.com>
2026-04-15 17:04:26 -0400

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,11 @@
+# show your webpage
+# accept uploaded audio files
+# call the transcription code
+# send note data back as JSON
+
+def main():
+    print("Hello from guitar-zero!")
+
+
+if __name__ == "__main__":
+    main()
